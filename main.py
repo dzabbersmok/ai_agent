4368c7b72c2963616a87ec14e035e3b008c0e367
ka3vas <tyrnis@gmail.com>
2025-06-24 17:30:26 +0200

    8

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def generate_content(client, messages, verbose_flag):
         print("Response tokens:", response.usage_metadata.candidates_token_count)
         
     print("Response:")
-    if response.function_calls is not None:
+    if response.function_calls is not None and len(response.function_calls) > 0:
         for function_call in response.function_calls:
             print(f"Calling function: {function_call.name}({function_call.args})")
     else:
